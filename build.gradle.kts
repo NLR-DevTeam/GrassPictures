73d3e8fb3daa81935e47e3a498948264c90dd0c4
XIAYM-gh <xiaym123@qq.com>
2023-03-26 19:30:21 +0800

    1.2.0 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "cn.whitrayhb"
-version = "1.1.9"
+version = "1.2.0"
 
 repositories {
     if (System.getenv("CI")?.toBoolean() != true) {
--- src/main/java/cn/whitrayhb/grasspics/GrasspicsMain.java
+++ src/main/java/cn/whitrayhb/grasspics/GrasspicsMain.java
@@ -34,7 +34,7 @@ public final class GrasspicsMain extends JavaPlugin {
     private static boolean usePublicPosting = false;
 
     private GrasspicsMain() {
-        super(new JvmPluginDescriptionBuilder("cn.whitrayhb.grasspics", "1.1.9")
+        super(new JvmPluginDescriptionBuilder("cn.whitrayhb.grasspics", "1.2.0")
                 .name("草图插件")
                 .info("草图适配插件")
                 .author("NLR DevTeam")
